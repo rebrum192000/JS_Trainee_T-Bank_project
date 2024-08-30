519cbe41e100b81efcf3a95b10996f30bcd0b459
Konstantin Nikitin <rebrum192000@gmail.com>
2024-08-30 12:19:19 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'JS_Trainee_T-Bank_project';
+}
